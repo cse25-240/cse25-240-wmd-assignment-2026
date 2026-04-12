82c1c88128b36610e8488a8edb7a6f12b8502514
MalMal010111 <cse25-240@thuto.bac.ac.bw>
2026-04-12 21:11:51 +0200

    index.html styles.css script.js

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
-
+/**header scroll effect */
+const /**{node element} */ $header = document.querySelector("[data-header]");
+window.addEventListener("scroll",e => {
+    $header.classList[window.scrollY > 50 ? "add" : "remove"]("active")})
